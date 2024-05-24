2ce205b4c4c381fae1a4cfc68eba56f49a0a3548
Agustin-Galarza <agustin.galarza96@gmail.com>
2024-05-24 20:30:26 -0300

    test getByType funcionando

--- src/main/java/com/codechallege/mendel/controller/TransactionController.java
+++ src/main/java/com/codechallege/mendel/controller/TransactionController.java
@@ -18,6 +18,21 @@ public class TransactionController {
         return new ResponseEntity<>("El servicio inserto correctamente", HttpStatus.CREATED);
     }
 
-
+    @GetMapping("/getTransactionByType")
+    ResponseEntity<List<Transaction>> getTransactionByType(@RequestParam String type){
+        //TODO: generar el servicio
+        List<Transaction> transactionList = new ArrayList<>();
+        Transaction transaction1 = new Transaction("1", "payment", "100.5", "2");
+        Transaction transaction2 = new Transaction("2", "payment", "101.5", "1");
+        Transaction transaction3 = new Transaction("3", "payment", "102.5", "2");
+        Transaction transaction4 = new Transaction("4", "payment", "103.5", "1");
+        Transaction transaction5 = new Transaction("5", "payment", "104.5", "1");
+        transactionList.add(transaction1);
+        transactionList.add(transaction2);
+        transactionList.add(transaction3);
+        transactionList.add(transaction4);
+        transactionList.add(transaction5);
+        return ResponseEntity.ok(transactionList);
+    }
 
 }
