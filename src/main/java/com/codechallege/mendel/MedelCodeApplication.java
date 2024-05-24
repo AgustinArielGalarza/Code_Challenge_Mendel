a57b2bf0df2ba1894faf14b014e77e6c7f604543
Agustin-Galarza <agustin.galarza96@gmail.com>
2024-05-24 17:08:42 -0300

    Commit inicial

---
+++ src/main/java/com/codechallege/mendel/MedelCodeApplication.java
@@ -0,0 +1,13 @@
+package com.codechallege.mendel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedelCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedelCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codechallege/mendel/MedelCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codechallege.mendel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedelCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
